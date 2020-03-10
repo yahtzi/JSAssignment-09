b07ddcbf61a151488c324ed8b75912088b59f63b
yahtzi <yaritzabejarano7220@my.whccd.com>
2020-03-09 19:09:33 -0700

    I

--- script.js
+++ script.js
@@ -4,19 +4,19 @@
 console.log(
   "What worked well for you over the last 9 weeks that aided in your learning process?"
 );
-console.log("");
+console.log("I structure of the class helped me a lot in the learning process. The flow of having a lecture, followed by an assignment, then having a homework workshop was a great structure for me.");
 
 // 2. Write a console.log() statement that answers the following question: What would you
 // change about the course?
 
 console.log("What would you change about the course?");
-console.log("");
+console.log("I don't think I would change much about the course. Any homework issues I had I was always able to text questions or ask questions in class for. Everything is set up to have as little struggle as possible.");
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
 
 console.log("Did you learn what you expected to learn in this course?");
-console.log("");
+console.log("I didn't know what to expect from this course, so having learned what I did I was good I suppose. I wish I was a little more proficient but I know that comes with practice.");
 
 // 4. Write a console.log() statement that answers the following question: What concepts
 // were new to you and what concepts had you encountered already in the past?
@@ -24,4 +24,4 @@ console.log("");
 console.log(
   "What concepts were new to you and what concepts had you encountered already in the past?"
 );
-console.log("");
+console.log("Loops were definitely something new I encountered in the course. Something I already knew from the past was linking something from the HTML to another page.");
